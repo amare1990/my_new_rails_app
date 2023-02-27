185835da6893b182ae1eb482cee4c1edcde02e9e
amare1990 <amaremek@gmail.com>
2023-02-28 01:15:49 +0300

    Create Greeting model, migrations and seed tables with five messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create!(message: 'Hello world')
+Greeting.create!(message: 'Hello rails')
+Greeting.create!(message: 'Hello react')
+Greeting.create!(message: 'Hello react-rails')
+Greeting.create!(message: 'Hello react-rails as a single app')
+
